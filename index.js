94184d5260ba256901026a7e1d307f271bff5955
EikWSS <stubbenspille@gmail.com>
2025-10-08 12:35:45 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+let myAPI = "https://dattebayo-api.onrender.com/characters"
+
+let inputSearch = document.getElementById("myInput")
+let inputButton = document.getElementById("myBtn")
+
+const charImage = document.getElementById("charImage");
+const charName = document.getElementById("Name");
+const Traits = document.getElementById("Traits")
+const Birthday = document.getElementById("Birthday");
+const clan = document.getElementById("clan")
+let Sharingan = document.getElementById("Sharingan")
+const uniqueTraits = document.getElementById("uniqueTraits")
+
+inputButton.addEventListener("click", function () {
+    fetch(myAPI)
+        .then(response => response.json())
+        .then(data => {
+            data.characters.forEach(character => {
+                if (inputSearch.value.toLowerCase() == character.name.toLowerCase()) {
+                    imageContainer.style.display = "flex"
+                    theTable.style.display = "flex"
+
+                    charImage.src = character.images[0]
+                    
+                    charImage.innerHTML = character.name
+                    Traits.innerHTML = character.personal.classification
+                    Birthday.innerHTML = character.personal.birthdate
+                    occupation.innerHTML = character.personal.occupation
+                    clan.innerHTML = character.personal.clan
+                    Sharingan.innerHTML = character.personal.kekkeiGenkai
+                    uniqueTraits.innerHTML = character.uniqueTraits
+                }
+            })
+        })
+})
+
+
+
+
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,19 @@
+#  Naruto Karaktersøk
+
+Et webprosjekt der du kan søke etter dine favorittkarakterer fra **Naruto** og se detaljer som evner, landsby, rang og mer!
+
+##  Funksjoner
+-  **Karaktersøk:** Finn Naruto-karakterer raskt ved å søke på navn.  
+-  **Karakterinfo:** Se detaljer som alder, rang, landsby, jutsu og klan.  
+-  **Rent design:** Enkelt og responsivt brukergrensesnitt.  
+-  **Raske resultater:** Drevet av et Naruto-karakterdata-API.
+
+##  Teknologistakk
+- **Frontend:** HTML, CSS, JavaScript   
+- **Backend:** javascript 
+- **API:** [let myAPI = "https://dattebayo-api.onrender.com/characters"]
+
+##  Installasjon og oppsett
+1. Klon prosjektet:
+   ```bash
+   git clone https://github.com/brukernavn/naruto-karaktersok.git
